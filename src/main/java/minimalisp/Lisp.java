ef7306f0e7446abc51b9e486a16bc5d137707608
Craig Davidson <craigmdavidson@gmail.com>
2017-06-05 17:40:16 +0100

    Removed unused import

--- src/main/java/minimalisp/Lisp.java
+++ src/main/java/minimalisp/Lisp.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
